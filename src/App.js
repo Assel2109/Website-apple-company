78d2368481fe95a4e1461e976aa4a2abd82b8e32
Assel S <120194838+Assel2109@users.noreply.github.com>
2023-03-26 23:26:35 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Items from "./components/Items"
 
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
-import Navbar from "./component/Header/Navbar"
-import Main  from "./component/Main/main"
-import Footer from "./component/Footer/Footer"
-
-import IPhone from "./component/Pages/IPhone/IPhone"
-import Error from "./component/Pages/Error/Error"
-import Mac from './component/Pages/Mac/Mac';
-import Watch from './component/Pages/Watch/Watch'
-import Productpage from './component/Pages/Productpage/Productpage'
-
-class App extends Component {
-  render(){
-    return (
-      <Router>
-        <div>
-          <Navbar/>
-            <Switch>
-               <Route path="/" exact component={Main}/>
-               <Route path="/iphone" exact component={IPhone}/>
-               <Route path="/mac" exact component={Mac}/>
-               <Route path="/watch" exact component={Watch}/>
-               <Route path="/iphone/:pid" exact component={Productpage}/>
-               <Route path="/" component={Error}/>
-            </Switch>          
-          <Footer/>
-        </div>
-      </Router>
-      
-    );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Kerapoxy Epoxy Grout 2 kg',
+          img: 'KERAPOXY EPOXY GROUT.jpg',
+          desc: 'Kerapoxy Epoxy Grout 2kg',
+          category: 'building mixes',
+          price: '76,75'
+        },
+        {
+          id: 2,
+          title: 'Putty Danogipssuperfinish 28 kg',
+          img: 'PUTTY DANOGIPSSUPERFINISH.jpg',
+          desc: 'Putty Danogipssuperfinish 28 kg',
+          category: 'building mixes',
+          price: '40'
+        },
+        {
+          id: 3,
+          title: 'Polymer Moisture-Resistant Putty Bergauf Uni Pasta 25 kg',
+          img: 'POLYMER MOISTURE-RESISTANT PUTTY BERGAUF UNI PASTA.jpg',
+          desc: 'Polymer Moisture-Resistant Putty Bergauf Uni Pasta 25 kg',
+          category: 'building mixes',
+          price: '37,38'
+        },
+        {
+          id: 4,
+          title: 'Grout Bergauf Elast Polymer Goth. Polym. for Interplate Seams Beige 2 kg',
+          img: 'GROUT BERGAUF ELAST POLYMER GOTH. POLYM. FOR INTERPLATE SEAMS BEIGE.jpg',
+          desc: 'Grout Bergauf Elast Polymer Goth. Polym. for Interplate Seams Beige 2 kg',
+          category: 'building mixes',
+          price: '30,05'
+        },
+        {
+          id: 5,
+          title: 'Glue D Tiles FLX24 (Fleksmortel) White 25 kg',
+          img: 'GLUE D TILES FLX24 (FLEKSMORTEL) WHITE.jpg',
+          desc: 'Glue D Tiles FLX24 (Fleksmortel) White 25 kg',
+          category: 'building mixes',
+          price: '22,7'
+        },
+        {
+          id: 6,
+          title: 'Grouting For Seams Epoxyelite E.10 Cocoa 1kg',
+          img: 'GROUTING FOR SEAMS EPOXYELITE E.10 COCOA.jpg',
+          desc: 'Grouting For Seams Epoxyelite E.10 Cocoa 1kg',
+          category: 'building mixes',
+          price: '21,84'
+        },
+      ]
+    }
   }
-  
+  render() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Items items={this.state.items} />
+      <Footer />
+      </div>
+  );
+}
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+   <header>
+<div>
+    <span className='logo'>Repair materials</span>
+    <ul className='nav'>
+    <li>About us</li>
+    <li>Shop</li>
+    <li>Contacts</li>
+    </ul>
+</div>
+<div className='presentation'></div>
+   </header> 
+  )
+}
